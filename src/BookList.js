faabfb6cc35b3dc1e97bf271ea6f796e6acd23ea
ChaimaeBouassab <shaimaabouassab@gmail.com>
2023-09-29 20:28:03 +0100

    donedone

--- src/BookList.js
+++ src/BookList.js
@@ -2,22 +2,29 @@
 
 import React from 'react';
 import { Card, Container, Row, Col } from 'react-bootstrap';
-
+const b=[ 
+  { coverImg: '/hihihi.png'}
+]
 const books = [
   {
-    title: 'Book 1',
-    author: 'Author 1',
-    coverImg: '/hihihi.png',
+    title: 'NEW-12 Rules for Life: An Antidote to Chaos',
+    author: 'Jordan Peterson',
+    coverImg: '/41zGXwxH-ZL.jpg',
   },
   {
-    title: 'Book 2',
-    author: 'Author 2',
-    coverImg: 'url_to_cover_image_2.jpg',
+    title: ' Critical Responses ',
+    author: 'Jordan Peterson',
+    coverImg: '/716M0udarJL._AC_UF1000,1000_QL80_.jpg',
   },
   {
-    title: 'Book 3',
-    author: 'Author 3',
-    coverImg: 'url_to_cover_image_3.jpg',
+    title: 'Beyond Order',
+    author: 'Jordan Peterson',
+    coverImg: '/71EYOo8+bfL._AC_UF1000,1000_QL80_.jpg',
+  },
+  {
+    title: 'The Different Jordan Peterson:',
+    author: 'Jordan Peterson',
+    coverImg: '/61v5S326HBL._AC_UF1000,1000_QL80_.jpg',
   },
 ];
 
